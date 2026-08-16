48a3d6c534f7c92a225c53946905026eccdb99c5
Anilson Lopes <nissobmx@gmail.com>
2026-08-15 21:13:49 -0300

    feat: enable analytics in development

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+if (import.meta.env.DEV) {
+  const { createGtag } = await import('vue-gtag')
+
+  app.use(createGtag({
+    tagId: 'G-K1BE8B5GBQ'
+  }))
+}
+
+app.mount('#app')
